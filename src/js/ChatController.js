21024315379cc00d0f870fdcd07ab552a5897d0a
Olga Runova <ockaaa@gmail.com>
2021-01-07 01:59:12 +0300

    first commit

---
+++ src/js/ChatController.js
@@ -0,0 +1,57 @@
+/* eslint-disable class-methods-use-this */
+const moment = require('moment');
+
+export default class ChatController {
+  constructor(container) {
+    this.container = container;
+    this.textarea = this.container.querySelector('.textarea');
+    this.users = this.container.querySelector('.users__list');
+    this.messages = this.container.querySelector('.messages__list');
+  }
+
+  addUser(user) {
+    const li = document.createElement('li');
+    li.classList.add('user__item');
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('img');
+    imgDiv.innerHTML = '<img src="./src/img/ava.jpg" alt="picture" class="user__img">';
+    const name = document.createElement('span');
+    name.classList.add('user__name');
+    name.textContent = user;
+    li.append(imgDiv);
+    li.append(name);
+    this.users.append(li);
+  }
+
+  addMessage(user, timestap, message) {
+    const li = document.createElement('li');
+    li.classList.add('message__item');
+    const header = document.createElement('header');
+    header.classList.add('message__header');
+
+    if (user === 'you') {
+      li.classList.add('message__your');
+      header.classList.add('message__header-your');
+    }
+    const name = document.createElement('span');
+    name.classList.add('message__name');
+    name.textContent = `${user}, `;
+    const time = document.createElement('time');
+    time.classList.add('message__time');
+    time.textContent = timestap;
+    header.append(name);
+    header.append(time);
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.textContent = message;
+    li.append(header);
+    li.append(div);
+    this.messages.append(li);
+  }
+
+  getDate() {
+    const now = moment();
+    const date = now.format('DD.MM.YY hh:mm');
+    return date;
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,67 @@
+import ChatController from './ChatController';
+
+const ws = new WebSocket('ws://ahj-ws.herokuapp.com');
+const chatCtrl = new ChatController(document.querySelector('.container'));
+
+const form = document.querySelector('.form');
+const textarea = document.querySelector('.textarea');
+let nickname = null;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const body = { type: 'authorization', name: form.name.value };
+  ws.send(JSON.stringify(body));
+});
+
+textarea.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    const body = {
+      type: 'message', user: nickname, time: chatCtrl.getDate(), message: textarea.value,
+    };
+    ws.send(JSON.stringify(body));
+    textarea.value = '';
+  }
+});
+
+ws.onmessage = (response) => {
+  const data = JSON.parse(response.data);
+
+  if (data.type === 'authorization') {
+    if (!data.name) {
+      document.querySelector('.mistake').classList.add('active');
+    } else {
+      nickname = data.name;
+      form.name.value = '';
+      document.querySelector('.modal').classList.add('hidden');
+      document.querySelector('.chat').classList.remove('hidden');
+    }
+  }
+
+  if (data.type === 'message') {
+    const { user, time, message } = data;
+    if (user === nickname) {
+      chatCtrl.addMessage('you', time, message);
+    } else {
+      chatCtrl.addMessage(user, time, message);
+    }
+  }
+
+  if (!data.type) {
+    chatCtrl.users.innerHTML = '';
+    data.forEach((elem) => {
+      if (elem === nickname) {
+        chatCtrl.addUser('You');
+      } else {
+        chatCtrl.addUser(elem);
+      }
+    });
+  }
+};
+
+window.onbeforeunload = () => {
+  const body = { type: 'disconnect', name: nickname };
+  ws.send(JSON.stringify(body));
+  ws.close();
+};
